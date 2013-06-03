f8713cfd0078e5865c00d198d32741ac472f3a75
Chris Uzelac <cuzelac@gmail.com>
2013-06-03 16:05:21 -0700

    Initial Commit

---
+++ AVAudioPlayer/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  AVAudioPlayer
+//
+//  Created by Chris Uzelac on 6/3/13.
+//  Copyright (c) 2013 allecto.org. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
